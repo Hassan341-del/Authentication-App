135cd249be1424039d1b789e5b94d98b39041106
Hassan Ali <hassanalibhatti648@gmail.com>
2024-05-02 13:57:51 +0500

    Changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-     <footer class="bg-body-tertiary text-center text-lg-start">
+     <footer class="bg-light text-center text-lg-start">
         <div class="text-center p-3">
           © 2024 Copyright: 
           <a class="text-body" href="#">tectsoft.com/</a>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,14 +8,14 @@ export default function Header(props) {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid justify-content-end">
+        <div className="container-fluid">
           <Link to="#" className="navbar-brand">Authentication App</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-            <Link to="/" className="nav-link">Home</Link>
+           <Link to="/" className="nav-link">Home</Link>
             <Link to="/about-us" className="nav-link">About Us</Link>
             <Link to="/services" className="nav-link">Services</Link>
             <Link to="/contact-us" className="nav-link">Contact Us</Link>
